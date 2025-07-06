a4d8db34d65eb41de3d1ff8f15d18f8389a903ca
Luiz Felipe Fontes Botelho <luizfelipefb@gmail.com>
2025-07-06 00:54:51 -0300

    feat(backup): add retention policy to backups

--- backup/backup.sh
+++ backup/backup.sh
@@ -11,11 +11,15 @@ export RESTIC_PASSWORD=""
 export PATH="/home/luizfelipefb/.asdf/shims:$PATH"
 
 # set local restic repository and perform backup
+echo -e "\n-> Starting backup to local repository"
 export RESTIC_REPOSITORY="/mnt/pool/backup"
 restic backup /home/luizfelipefb/appdata/
-echo -e "-> Backup to local repository completed\n"
+echo -e "\n-> Applying retention policy to local repository"
+restic forget --keep-daily 7 --keep-weekly 4 --keep-monthly 12 --keep-yearly 3 --prune
 
 # set remote restic repository and perform backup
+echo -e "\n-> Starting backup to remote repository"
 export RESTIC_REPOSITORY="s3:s3.us-east-1.amazonaws.com/squats-backup"
 restic backup /home/luizfelipefb/appdata/
-echo "-> Backup to remote repository completed\n"
+echo -e "\n-> Applying retention policy to local repository"
+restic forget --keep-daily 7 --keep-weekly 4 --keep-monthly 12 --keep-yearly 3 --prune
